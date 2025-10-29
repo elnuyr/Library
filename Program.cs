26dbc6a5fcfcd1d83c00b702368f7abeabe794ec
elnuyr <elnuren-azmpa201@code.edu.az>
2025-10-30 01:42:45 +0400

    first

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Model;
+Employee employee = new Employee("Elnur", true, 500);
+Assistant assistant = new Assistant("Kamran", true, 1000);
+
+Console.WriteLine($"Before : {employee.Name} - Salary: {employee.Salary}");
+assistant.GetFeedback(employee);
+Console.WriteLine($"After : {employee.Name} - Salary: {employee.Salary}");
+
+Console.WriteLine($"Assistant Ad : {assistant.Name} - Salary: {assistant.Salary}");
